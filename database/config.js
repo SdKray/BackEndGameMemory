360841eeed45a8693afbeced16242baf1b689371
Juan Palo <spokendrack_123@hotmail.com>
2021-06-08 16:23:09 -0500

    Conection to database created

---
+++ database/config.js
@@ -0,0 +1,18 @@
+// atajo req ,rqr
+const mongose = require('mongoose');
+
+const dbConnection = async () => {
+	try {
+		mongose.connect(process.env.DB_CONECTION, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('Data base on line');
+	} catch (error) {
+		console.log(error);
+		throw new Error('Error en la conexion');
+	}
+};
+
+module.exports={dbConnection}
--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // TODO: Levantar el server
+const express = require('express');
+require('dotenv').config();
+const app = express();
+//Lectura y parseo del body
+app.use(express.json());
+// Rutas
+app.get('/home', (req, res) => {
+	console.log(req.body.nombre);
+	res.status(200).json({
+		ok: true,
+		msj: 'Felicidad encontraste home ',
+		type: 'GET',
+	});
+});
+
+app.post('/home', (req, res) => {
+	console.log('estas dentro de la ruta home');
+	res.status(200).json({
+		ok: true,
+		msj: 'Felicidad encontraste home ',
+		type: 'POST',
+	});
+});
+
+app.delete('/home', (req, res) => {
+	console.log('estas dentro de la ruta home');
+	res.status(200).json({
+		ok: true,
+		msj: 'Felicidad encontraste home ',
+		type: 'DELETE',
+	});
+});
+
+app.put('/home', (req, res) => {
+	console.log('estas dentro de la ruta home');
+	res.status(200).json({
+		ok: true,
+		msj: 'Felicidad encontraste home ',
+		type: 'PUT',
+	});
+});
+
+app.listen(4000, () => {
+	console.log(process)
+    console.log('El servidor esta corriendo en el puerto 4000');
+});
+
