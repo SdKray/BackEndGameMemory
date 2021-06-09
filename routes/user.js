12cfde5248d543cc8ec4d8d6eaa8ddb536f3b868
Luis Alberto <luisbc_1998@hotmail.com>
2021-06-09 10:52:44 -0500

    routes user fixed

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+// * Se importa el Router de express
+const { Router } = require('express');
+// * Se asignan los metodos a "router"
+const router = Router();
+
+// * Validar datos desde la peticion con express-validator
+const { check } = require('express-validator');
+
+
+// * Se establecen las rutas con las funciones get, post, put o delete
+router.post('/new', (req, res) => {
+	console.log(req.body.nombre);
+	res.status(200).json({
+		ok: true,
+		msj: 'Holi',
+		type: 'Get',
+	});
+});
+
+// * se exporta el router para su uso en el index
+module.exports = router
