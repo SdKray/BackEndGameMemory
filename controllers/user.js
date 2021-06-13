b373e52326b0ac04920880e67846b4dab1733a76
Luis Alberto <luisbc_1998@hotmail.com>
2021-06-13 17:15:22 -0500

    helpers added and login feature created

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const { request, response } = require('express');
 const User = require('../models/User');
 const bcryptjs = require('bcryptjs');
+const { generarJWT } = require('../helpers/jwt');
 
 const createUser = async (req = request, res = response) => {
 	const { email, password } = req.body;
@@ -17,14 +18,16 @@ const createUser = async (req = request, res = response) => {
 		const salt = bcryptjs.genSaltSync();
 		user.password = bcryptjs.hashSync(password, salt);
 
-		await user.save()
+		await user.save();
 
 		// TODO: Generar Token
+		const token = await generarJWT(user._id, user.name);
 
 		res.status(201).json({
 			ok: true,
 			uid: user.id,
 			name: user.name,
+			token,
 		});
 	} catch (error) {
 		console.log(error);
@@ -36,4 +39,53 @@ const createUser = async (req = request, res = response) => {
 	return;
 };
 
-module.exports = { createUser };
+const loginUser = async (req = request, res = response) => {
+	const { email, password } = req.body;
+	try {
+		let user = await User.findOne({ email });
+		if (!user) {
+			res.status(400).json({
+				ok: false,
+				msg: 'No se encontro a ningun usuario con el email',
+			});
+		}
+
+		//Validar password con bcryptjs
+		const validPassword = bcryptjs.compareSync(password, user.password);
+		if (!validPassword) {
+			res.status(400).json({
+				ok: false,
+				msg: 'Contraseña incorrecta',
+			});
+		}
+		const token = await generarJWT(user._id, user.name);
+
+		res.status(200).json({
+			ok: true,
+			uid: user.id,
+			name: user.name,
+			token,
+		});
+	} catch (error) {
+		console.log('[login]', error);
+		return res.status(500).json({
+			ok: false,
+			msg: 'Por favor Hable con el administrador',
+		});
+	}
+};
+
+const revalidarToken =async (req = request, res = response) => {
+	const { uid, name } = req;
+
+	const token = await generarJWT(uid, name);
+
+	res.json({
+		ok: true,
+		token,
+		uid,
+		name,
+	});
+};
+
+module.exports = { createUser, loginUser };
--- routes/user.js
+++ routes/user.js
@@ -5,10 +5,12 @@ const router = Router();
 
 // * Validar datos desde la peticion con express-validator
 const { check } = require('express-validator');
-const { createUser } = require('../controllers/user');
+const { createUser, loginUser } = require('../controllers/user');
 
 // * Se establecen las rutas con las funciones get, post, put o delete
 router.post('/new',createUser);
+
+router.post('/',loginUser);
 
 // * se exporta el router para su uso en el index
 module.exports = router;
